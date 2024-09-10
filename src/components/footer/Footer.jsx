25ec063d61b7c94a9c2caeb30606ba21959badf6
anti00700 <aniruddhtiwari594@gmail.com>
2024-09-10 10:06:23 +0530

    intial commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./footer.css";
+import { AiFillFacebook, AiFillTwitterSquare, AiFillInstagram, } from "react-icons/ai";
+
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="footer-content">
+        <p>
+          &copy; 2024 Anishruti. All rights reserved. <br /> 
+          Made with ❤️ by <a href="">Anirudh</a>
+        </p>
+        <div className="social-links">
+          <a href=""><AiFillFacebook />
+          </a>
+          <a href=""><AiFillTwitterSquare /></a>
+          <a href=""><AiFillInstagram />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
